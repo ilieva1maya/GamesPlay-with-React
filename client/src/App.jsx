2f5a60d2468f8458a1eed3a7a3c44b3897d9a9ab
Maya Ilieva <ilieva1maya@gmail.com>
2024-04-19 15:46:18 +0300

    Initial routing added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,24 +1,28 @@
 import Catalog from "./components/catalog/Catalog"
+import Create from "./components/create/Create"
 import Details from "./components/details/Details"
 import Edit from "./components/edit/Edit"
 import Header from "./components/header/Header"
 import Home from "./components/home/Home"
 import Login from "./components/login/Login"
 import Register from "./components/register/Register"
+import { Routes, Route } from "react-router-dom"
 
 function App() {
-
   return (
     <>
       <div id="box">
         <Header />
-        <Home />
-        <Catalog />
-        <Details/>
-        <Edit />
-        <Login />
-        <Register />
-        </div>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/catalog' element={<Catalog />} />
+          <Route path='/create' element={<Create />} />
+          <Route path='/details' element={<Details />} />
+          <Route path='/edit' element={<Edit />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/register' element={<Register />} />
+        </Routes>
+      </div>
     </>
   )
 }
