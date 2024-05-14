226dd21114ad371a25da108838f1b8d2e83daa9a
Maya Ilieva <ilieva1maya@gmail.com>
2024-05-14 15:10:58 +0300

    App.js single responsibility

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,3 @@
-import { useState } from "react"
 import Catalog from "./components/catalog/Catalog"
 import Create from "./components/create/Create"
 import Details from "./components/details/Details"
@@ -6,57 +5,15 @@ import Edit from "./components/edit/Edit"
 import Header from "./components/header/Header"
 import Home from "./components/home/Home"
 import Login from "./components/login/Login"
-import Register from "./components/register/Register"
-import { Routes, Route, useNavigate } from "react-router-dom"
-import { AuthProvider } from "./contexts/authContext"
-
-import * as authService from "./services/authService"
-import Path from "./paths"
 import Logout from "./components/logout/Logout"
-// import AuthContext from "./contexts/authContext"
+import Register from "./components/register/Register"
+import { Routes, Route } from "react-router-dom"
+import { AuthProvider } from "./contexts/authContext"
+import Path from "./paths"
 
 function App() {
-  const navigate = useNavigate();
-  const [auth, setAuth] = useState(() => {
-    localStorage.removeItem('accessToken');
-    return {};
-  });
-
-  const loginSubmitHandler = async (values) => {
-    const result = await authService.login(values.email, values.password);
-    // струва ми се, че трябва да е същото, но не се получава, трябва да видя защо
-    // const result = await authService.login(Object.values(values))
-
-    setAuth(result);
-    localStorage.setItem('accessToken', result.accessToken);
-    navigate(Path.Home);
-  }
-
-  const registerSubmitHandler = async (values) => {
-    const result = await authService.register(values.email, values.password);
-
-    setAuth(result);
-    localStorage.setItem('accessToken', result.accessToken);
-    navigate(Path.Home);
-  }
-
-  const logoutHandler = () => {
-    setAuth({});
-    localStorage.removeItem('accessToken');
-  }
-
-  const values = {
-    loginSubmitHandler,
-    registerSubmitHandler,
-    logoutHandler,
-    username: auth.username || auth.email,
-    email: auth.email,
-    isAuthenticated: !!auth.accessToken,
-  }
-
   return (
-    // <AuthContext.Provider value={values}>
-    <AuthProvider value={values}>
+    <AuthProvider>
       <div id="box">
         <Header />
         <Routes>
@@ -67,14 +24,11 @@ function App() {
           <Route path={Path.Edit} element={<Edit />} />
           <Route path={Path.Login} element={<Login />} />
           <Route path={Path.Logout} element={<Logout />} />
-          {/* ако не подаваме през context, а през props */}
-          {/* <Route path='/login' element={<Login loginSubmitHandler={loginSubmitHandler} />} /> */}
           <Route path={Path.Register} element={<Register />} />
           <Route path='/games-catalog/:id' element={<Details />} />
         </Routes>
       </div>
     </AuthProvider>
-    // {/* </AuthContext.Provider> */}
   )
 }
 
